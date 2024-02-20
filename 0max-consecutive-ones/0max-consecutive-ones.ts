26b67d7bbf39461ace3f4ce2baa4d1e81b37f0ab
Muhammad Rashid Ridha bin Muhammad Firhat <68490961+rashidfirhat98@users.noreply.github.com>
2024-02-21 01:26:18 +0800

    Time: 523 ms, Memory: 63 MB - LeetHub

---
+++ 0max-consecutive-ones/0max-consecutive-ones.ts
@@ -0,0 +1,24 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+  let consecutiveOnes: number = 0
+  const arrayOfConsecutiveOnes: number[] = []
+  let largest: number = 0
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] === 1) {
+        consecutiveOnes = consecutiveOnes + 1;
+        console.log(consecutiveOnes)
+      } else {
+        consecutiveOnes = 0
+      }
+      if (consecutiveOnes !== 0) {
+          arrayOfConsecutiveOnes.push(consecutiveOnes)
+      }
+    }
+  console.log(arrayOfConsecutiveOnes)
+  for (let j = 0; j < arrayOfConsecutiveOnes.length; j++) {
+    if (arrayOfConsecutiveOnes[j] > largest) {
+      largest = arrayOfConsecutiveOnes[j]
+    }
+  }
+  console.log(largest)
+  return largest
+};
