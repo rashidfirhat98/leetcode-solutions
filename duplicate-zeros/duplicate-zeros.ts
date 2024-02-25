10a88edd46d86a1acf1ce62f0b84a111bbc46522
Muhammad Rashid Ridha bin Muhammad Firhat <68490961+rashidfirhat98@users.noreply.github.com>
2024-02-26 00:30:50 +0800

    Time: 78 ms, Memory: 52.2 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.ts
+++ duplicate-zeros/duplicate-zeros.ts
@@ -2,11 +2,12 @@
  Do not return anything, modify arr in-place instead.
  */
 function duplicateZeros(arr: number[]): void {
-  for(let i = arr.length - 1; i >= 0; i--) {
+  for(let i = 0; i < arr.length; i++) {
     if(arr[i] === 0) {
       for(let j = arr.length - 1; j > i; j--) {
         arr[j] = arr[j - 1]
       }
+      i++
     }
   }
  
