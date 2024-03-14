3971a488a12bbf0c31f7e4e8a765d1918ec64329
Muhammad Rashid Ridha bin Muhammad Firhat <68490961+rashidfirhat98@users.noreply.github.com>
2024-03-14 18:31:03 +0800

    Time: 68 ms, Memory: 52.8 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,10 +1,11 @@
 function removeDuplicates(nums: number[]): number {
-  let l = 0
-    for(let r = 0; r <= nums.length; r++){
-      if(nums[l] !== nums[r]) {
-        l++
-        nums[l] = nums[r]
-      }
+  
+  let r = 1
+  for(let i = 0; i < nums.length - 1; i++) {
+    if(nums[i] !== nums[i + 1]) {
+      nums[r] = nums[i + 1]
+      r++
     }
-  return l
+  }
+  return r
 };
