da94fe2c311f0e667923fdd1bf1006808d94b49c
Muhammad Rashid Ridha bin Muhammad Firhat <68490961+rashidfirhat98@users.noreply.github.com>
2024-03-29 23:07:21 +0800

    Time: 61 ms (31.18%) | Memory: 51.3 MB (57.95%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.ts
@@ -1,6 +1,5 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
     const newArr: number[] = []
-
     for(let x = 0; x < arr.length; x++) {
         newArr[x] = fn(arr[x], x) 
     }
