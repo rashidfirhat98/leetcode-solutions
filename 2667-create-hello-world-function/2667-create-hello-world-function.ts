aad866dc79e2139d09fe573e0679398925f4349e
Muhammad Rashid Ridha bin Muhammad Firhat <68490961+rashidfirhat98@users.noreply.github.com>
2024-11-05 16:56:41 +0800

    Time: 58 ms (39.58%), Space: 49.7 MB (85.84%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.ts
@@ -0,0 +1,9 @@
+// const createHelloWorld = function() {
+//     return function(): string {
+//         return "Hello World";
+//     }
+// };
+
+const createHelloWorld = function() {
+    return () => "Hello World";
+}
